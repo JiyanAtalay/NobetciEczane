ffbb9eecb17b4ed8d227570c4a7028c552c1be48
JiyanAtalay <jyntly23@gmail.com>
2024-09-25 14:17:07 +0300

    Initial Commit

---
+++ NobetciEczane/NobetciEczaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NobetciEczaneApp.swift
+//  NobetciEczane
+//
+//  Created by Mehmet Jiyan Atalay on 25.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct NobetciEczaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NobetciEczaneTests/NobetciEczaneTests.swift
@@ -0,0 +1,17 @@
+//
+//  NobetciEczaneTests.swift
+//  NobetciEczaneTests
+//
+//  Created by Mehmet Jiyan Atalay on 25.09.2024.
+//
+
+import Testing
+@testable import NobetciEczane
+
+struct NobetciEczaneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
